a52230b36508662b2df2d900e496e3a6718ea09b
Dezeli <haterecursive@gmail.com>
2024-03-04 20:42:58 +0900

    Study

---
+++ Chapter1/Game.cpp
@@ -0,0 +1,71 @@
+﻿#include "Game.h"
+
+Game::Game() :
+	mWindow(nullptr),
+	mIsRunning(true)
+{
+
+}
+
+
+bool Game::Initialize()
+{
+	int sdlResult = SDL_Init(SDL_INIT_VIDEO);
+	if (sdlResult != 0)
+	{
+		SDL_Log("SDL initaialize Failed : %s", SDL_GetError());
+		return false;
+	}
+
+	wchar_t wKtitle[256] = L"게임 개발 연습 1";
+	char mKtitle[256];
+	WideCharToMultiByte(CP_UTF8, 0, wKtitle, -1, mKtitle, 256, NULL, NULL);
+
+	mWindow = SDL_CreateWindow(
+		mKtitle,
+		50,
+		50,
+		1200,
+		700,
+		0
+	);
+
+	if (!mWindow)
+	{
+		SDL_Log("Window Create Failed: %s", SDL_GetError());
+		return false;
+	}
+
+	return true;
+}
+
+void Game::RunLoop()
+{
+	while (mIsRunning)
+	{
+		ProcessInput();
+		UpdateGame();
+		GenerateOutput();
+	}
+}
+
+void Game::ProcessInput() 
+{
+
+}
+
+void Game::UpdateGame() 
+{
+
+}
+
+void Game::GenerateOutput() 
+{
+
+}
+
+void Game::Shutdown()
+{
+	SDL_DestroyWindow(mWindow);
+	SDL_Quit();
+}
---
+++ Chapter1/Game.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Windows.h"
+#include "SDL/SDL.h"
+#include "Stringapiset.h"
+
+class Game
+{
+public:
+	Game();
+
+	bool Initialize();
+
+	void RunLoop();
+
+	void Shutdown();
+
+private:
+	void ProcessInput();
+
+	void UpdateGame();
+
+	void GenerateOutput();
+
+	SDL_Window* mWindow;
+	bool mIsRunning;
+};
